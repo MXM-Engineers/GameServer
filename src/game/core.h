99eb02433d7d148ac5e30691abf1e49fea2da375
Christophe Huybrechts <huybchris@hotmail.com>
2020-09-26 02:06:51 +0200

    Add more types

--- src/game/core.h
+++ src/game/core.h
@@ -34,8 +34,63 @@ enum EntityType : i32
 {
 	ENTITY_INVALID = 0,
 	ENTITY_CREATURE = 1,
-	ENTITY_DYNAMIC = 2
+	ENTITY_DYNAMIC = 2,
+	ENTITY_SFX = 3,
+	ENTITY_ITEM = 4,
+	ENTITY_TERRAIN = 5 // haven't found this one in EntityComData was in an attribute CaseValue1 in tag <StatePlay>
+};
 
+// Has ItemComData _Type ITEM_TYPE_ETC
+// Most of these types don't have a subtype 
+enum EtcType : i32
+{
+	ETC_INVALID = 0,
+	ETC_SUPPORTKIT = 1,
+	ETC_GOLD = 2,
+	ETC_SOUL = 3,
+	ETC_INGREDIENT = 4,
+	ETC_TICKET = 5,
+	ETC_EATER = 6,
+	ETC_TITLE = 7,
+	ETC_EXPBOOSTER = 8,
+	ETC_RONBOOSTER = 9,
+	ETC_REVIVAL_BY_COIN = 10,
+	ETC_OTHERS = 11,
+	ETC_OPEN_REWARD_ONEMORE = 12,
+	ETC_OPEN_REWARD_ALL = 13,
+	ETC_PROMOTIONGOLD = 14,
+	ETC_SKIN = 15,
+	ETC_SKINSTONE = 16,
+	ETC_FOOD = 17,
+	ETC_GUILD_EMBLEM = 18,
+	ETC_JUKEBOX = 19,
+	ETC_LEVELUPCOIN = 20,
+	ETC_LIFETIME_SKIN = 21,
+	ETC_LIFECHARGER = 22,
+	ETC_MEDALBOOSTER = 23,
+	ETC_CCOINTOGOLD = 24,
+	ETC_CHARACTER = 25,
+	ETC_BOOTY = 26,
+	ETC_BOX = 27,
+	ETC_NONE = 28
+};
+
+// Has EntityComData _Type ENTITY_TYPE_ITEM
+enum ItemType : i32
+{
+	ITEM_INVALID = 0,
+	ITEM_WEAPON = 1,
+	ITEM_ETC = 2,
+	ITEM_GEAR = 3
+};
+
+// Has ItemComData _Type ITEM_TYPE_GEAR
+enum GearType : i32
+{
+	GEAR_INVALID = 0,
+	GEAR_ATT = 1,
+	GEAR_DEF = 2,
+	GEAR_UTIL = 3
 };
 
 // NPC's that have shops or the jukebox have also _NPCType in CreatureComData
@@ -61,6 +116,13 @@ enum MapType: i32
 	MAP_INVALID = 0,
 	MAP_CITY = 1, //LOBBY
 	MAP_INGAME = 2
+};
+
+// Has ETC_TYPE_SUPPORTKIT _Type in EtcItemComData
+enum SupportKitType : i32
+{
+	SUPPORTKIT_INVALID = 0,
+	SUPPORTKIT_HP = 1
 };
 
 // When the maptype is INGAME it has a GameSubModeType except the training room

3f5af48a70451d5ec7b2dd9692cb6f007f23982b
LordSk <lordskelethom@gmail.com>
2021-05-03 15:56:27 +0200

    Displace capsule along triangle normal

--- src/game/debug/window.cpp
+++ src/game/debug/window.cpp
@@ -418,21 +418,19 @@ struct CollisionTest
 				DrawVec(-pen.dir * pen.depth, sphereCenter, vec3(1, 1, 0));
 
 				if(bShowFixedCapsule) {
-					vec3 d1 = capsuleA.InnerBase() - sphereCenter;
-					vec3 d2 = capsuleA.InnerTip() - sphereCenter;
-					vec3 p = -pen.dir * pen.depth;
+					vec3 p = sphereCenter + pen.dir * (capsuleA.radius - pen.depth);
+					Draw(p, vec3(1, 0.8, 0));
+					vec3 pp = glm::dot(p - sphereCenter, triangleB.Normal()) * triangleB.Normal();
+					DrawVec(pp, sphereCenter, vec3(0.5, 1, 0.5));
+					vec3 pp2 = triangleB.Normal() * capsuleA.radius + pp;
+					DrawVec(pp2, sphereCenter + -triangleB.Normal() * capsuleA.radius, vec3(0.5, 0.5, 1));
 
-					if(LengthSq(d1) < LengthSq(d2)) {
-						p -= d1;
-					}
-					else {
-						p -= d2;
-					}
+					pp2 -= capsuleA.InnerBase() - sphereCenter;
 
 					PhysCapsule fixed = capsuleA;
-					fixed.base += p;
-					fixed.tip += p;
-					Draw(fixed, vec3(1, 0.5, 0));
+					fixed.base += pp2;
+					fixed.tip += pp2;
+					Draw(fixed, vec3(0.5, 1, 0.5));
 				}
 			}
 			else {
